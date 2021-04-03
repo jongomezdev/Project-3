82d55b027d1140967e54f0f5e27bdae5f0ec0478
Jonathan Gomez <jongomezdev@gmail.com>
2021-04-03 16:27:38 -0400

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <p>hello world</p>
+    </>
+  );
+}
+
+export default App;
