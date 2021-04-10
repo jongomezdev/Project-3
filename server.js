118ed02f859145785e399a14b965df66fce75864
Jonathan Gomez <jongomezdev@gmail.com>
2021-04-10 15:02:57 -0400

    changing logs

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const mongoose = require('mongoose');
 // const routes = require('./routes');
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ extended: false }));
 // Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'));
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 // Add routes, both API and view
 
 app.use('/api/users', require('./routes/api/users'));
