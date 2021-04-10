8c7cb8d0c07a9acfaf1fd322b45f1960d9994286
Greg Scott <gtscott90@gmail.com>
2021-04-10 13:54:27 -0400

    drafted LandingPage page

---
+++ client/src/pages/LandingPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+function LandingPage() {
+    return (
+        <div>
+            Placeholder text for landing page
+            <Link to="/login"> Placeholder link to login </Link>
+            <Link to="/register"> Placeholder link to register </Link>
+        </div>
+    )
+}
+
+export default LandingPage;
