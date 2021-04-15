e362c13c4ecf2e8ca6292f932a8d2bad98c01a96
Genis Bardales <genis96@yahoo.com>
2021-04-15 19:28:11 -0400

    push

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LandingPage from "./pages/LandingPage";
-// import Wrapper from "./components/Wrapper/index";
+import Wrapper from "./components/Wrapper/wrapper";
 import Navbar from "./components/Navbar/app";
+import Login from "./components/LoginCard/login";
+
 
 function App() {
   return (
     <Router>
     <div>
-      
-      
+      <Wrapper>
+      {/* <Login /> */}
       <Navbar />
       <h1>hello world</h1>
+      <Route exact path="/" component={Login} />
       <LandingPage />
-      
+      </Wrapper>
     </div>
     </Router>
   );
---
+++ client/src/pages/Login.js
@@ -0,0 +1,15 @@
+// import React, { Component } from "react";
+// import { Container } from "react-router-dom";
+
+// class Login extends Component {
+//     render() {
+//         return (
+//             <div className="container">
+//                 <h2>...TEST</h2>
+//             </div>
+//         )
+//     }
+// }
+
+// export default Login;
+
