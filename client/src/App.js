eb29e70c04ef01dbe5f96a5ea3b98cf93c6aa9f8
Jonathan Gomez <jongomezdev@gmail.com>
2021-04-15 19:21:08 -0400

    backend completed

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 // import Wrapper from "./components/Wrapper/index";
-// import Navbar from "./components/Navbar/app";
+import Navbar from './components/Navbar/app';
 
 function App() {
   return (
     <Router>
       <div>
-        {/* <Navbar /> */}
+        <Navbar />
         <h1>hello world</h1>
         <LandingPage />
       </div>
