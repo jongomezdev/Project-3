3c3fc7faf20c1a1252992876e5d50419acf46a3d
Jonathan Gomez <jongomezdev@gmail.com>
2021-04-17 12:27:47 -0400

    alert reducer

---
+++ client/src/reducers/alert.js
@@ -0,0 +1,21 @@
+import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
+
+const initialState = [
+  {
+    id: 1,
+    msg: '',
+    alertType: 'success',
+  },
+];
+
+export default function (state = initialState, action) {
+  const { type, payload } = action;
+  switch (type) {
+    case SET_ALERT:
+      return [...state, action.payload];
+    case REMOVE_ALERT:
+      return state.filter((alert) => alert.id !== payload);
+    default:
+      return state;
+  }
+}
