1f4fe02df9ec2ae029a384768c849ca452efb34d
Jonathan Gomez <jongomezdev@gmail.com>
2021-04-20 20:14:27 -0400

    create auth action

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,19 +1,108 @@
-// import React from "react";
-// import { connect } from 'react-redux';
-// import { setAlert } from '../../actions/alert';
-// import {makeStyles, CssBaseline, Button, Container, Avatar, Typography, TextField, FormControlLabel, Checkbox, Grid, Link} from '@material-ui/core';
+import React from 'react';
+import { connect } from 'react-redux';
+import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
+// import {
+//   makeStyles,
+//   CssBaseline,
+//   Button,
+//   Container,
+//   Avatar,
+//   Typography,
+//   TextField,
+//   FormControlLabel,
+//   Checkbox,
+//   Grid,
+//   Link,
+// } from '@material-ui/core';
 
 // const useStyles = makeStyles((styles) => ({
-//         // styles
+//   // styles
 // }));
 
-// function Register(props) {
-//     const classes = useStyles();
-//     return (
-//         <Container component="main" maxWidth="xs">
+const Register = ({ setAlert, register }) => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
 
-//         </Container>
-//     )
-// }
+  const { name, email, password, password2 } = formData;
 
-// export default connect(null, { setAlert })(Register);
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      setAlert('Passwords do not match');
+    } else {
+      register({ name, email, password });
+    }
+  };
+
+  return (
+    <>
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
+        <h1 className="large text-primary">Sign Up</h1>
+        <p className="lead">
+          <i className="fas fa-user" /> Create Your Account
+        </p>
+        <form className="form" onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              value={name}
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              placeholder="Email Address"
+              name="email"
+              value={email}
+              onChange={onChange}
+            />
+            <small className="form-text">
+              This site uses Gravatar so if you want a profile image, use a
+              Gravatar email
+            </small>
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              name="password2"
+              value={password2}
+              onChange={onChange}
+            />
+          </div>
+          <input type="submit" className="btn btn-primary" value="Register" />
+        </form>
+        <p className="my-1">
+          Already have an account? <Link to="/login">Sign In</Link>
+        </p>
+      </form>
+    </>
+  );
+};
+
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+};
+
+export default connect(null, { setAlert, register })(Register);
