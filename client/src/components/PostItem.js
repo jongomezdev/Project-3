22c28b58ad9cabc32c274ec63eb0c54e4f7815f1
Greg Scott <gtscott90@gmail.com>
2021-04-20 22:45:04 -0400

    working on newsfeed

--- client/src/components/PostItem.js
+++ client/src/components/PostItem.js
@@ -1,15 +1,12 @@
 import React from "react";
-
 //exports both the List and ListItem components
-
-export function  List({ children }) {
+export function List({ children }) {
     return (
         <div className="list-overflow-container">
             <ul className="list-group">{ children }</ul>
         </div>
     );
 }
-
-export function  PostItem({ children }) {
+export function  ListItem({ children }) {
     return <li className="list-group-item">{children}</li>;
 }
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,16 +1,15 @@
 import axios from "axios";
-
 export default {
   getPosts: function() {
     return axios.get(`/api/posts`);
   },
   getPost: function(id) {
-    return axios.get(`/api/posts/ ${id}`);
+    return axios.get(`/api/posts/${id}`);
   },
   deletePost: function(id) {
-    return axios.delete(`/api/posts/" ${id}`);
+    return axios.delete(`/api/posts/${id}`);
   },
   savePost: function(postData) {
-    return axios.post(`/api/posts, postData`);
+    return axios.post(`/api/posts`, postData);
   }
 };
