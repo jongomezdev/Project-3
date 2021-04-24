fd50d2864d4bde9e6077d476efcdffe650632566
Greg Scott <gtscott90@gmail.com>
2021-04-24 17:28:29 -0400

    added cancel token to make sure the params dont change too quickly and make too many axios calls

--- client/src/components/useFetchJobs.js
+++ client/src/components/useFetchJobs.js
@@ -27,12 +27,15 @@ export default function useFetchJobs(params, page) {
     const [state, dispatch] = useReducer(reducer, {jobs: [], loading: true })
 
     useEffect(() => {
+        const cancelToken = axios.CancelToken.source()
         dispatch({ type: ACTIONS.MAKE_REQUEST })
         axios.get(BASE_URL, {
+            cancelToken: cancelToken.token,
             params: { markdown: true, page: page, ...params }
         }).then(res => {
             dispatch({ type: ACTIONS.GET_DATA, payload: { jobs: res.data } })
         }).catch(err => {
+            if (axios.isCancel(err)) return
             dispatch({ type: ACTIONS.ERROR, payload: { error: err } })
         })
     }, [params, page])
