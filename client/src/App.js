ec7c8115ad720b9ba7a67af4fc2c0809309a93c1
Greg Scott <gtscott90@gmail.com>
2021-04-24 16:04:29 -0400

    wrote empty function in SearchResults, imported Search Results to JobSearch page, updated router in app.js so /search goes to the JobSearch page instead of the SearchForm component

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ import { Provider } from 'react-redux';
 import store from './store';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
+import JobSearch from './pages/Jobsearch';
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -42,7 +43,7 @@ function App() {
                   <Route exact path="/" component={LandingPage} />
                   <Route exact path="/login" component={Login} />
                   <Route exact path="/register" component={Register} />
-                  <Route exact path="/search" component={SearchForm} />
+                  <Route exact path="/search" component={JobSearch} />
                   <PrivateRoute exact path="/dashboard" component={Dashboard} />
                 </Switch>
                 <Switch>
