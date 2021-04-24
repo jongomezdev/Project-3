06066dc88ef66fdcb208172845f9f70bbe8e6fc0
Greg Scott <gtscott90@gmail.com>
2021-04-24 17:40:59 -0400

    job titles being rendered on the SearchResults component

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import useFetchJobs from "./useFetchJobs";
 import { Container } from "react-bootstrap";
+import Job from "./Job";
 
 export default function SearchResults() {
     const [params, setParams] = useState({})
@@ -10,7 +11,9 @@ return(
 <Container>
  {loading && <h1>Loading...</h1>}
  {error && <h1>Error. Try Refreshing.</h1>}
- <h1>{jobs.length}</h1>
+ {jobs.map(job => {
+     return <Job key={job.id} job={job} />
+ })}
 </Container>
 )
 }
