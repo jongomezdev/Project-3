f6f510984aa3fd224ca461b68a288f1f06857df5
Greg Scott <gtscott90@gmail.com>
2021-04-24 16:45:13 -0400

    wrote the actions for the reducer function

--- client/src/components/useFetchJobs.js
+++ client/src/components/useFetchJobs.js
@@ -1,6 +1,29 @@
-import React from "react";
+import { useReducer } from "react";
+
+const ACTIONS = {
+    MAKE_REQUEST: "make-request",
+    GET_DATA: "get-data",
+    ERROR: "error"
+}
+
+function reducer(state, action) {
+    switch (action.type) {
+        case ACTIONS.MAKE_REQUEST:
+            return { loading: true, jobs: [] }
+        case ACTIONS.GET_DATA:
+            return { ...state, loading: false, jobs: action.payload.jobs }
+        case ACTIONS.ERROR:
+            return { ...state, loading: false, error: action.payload.error, jobs: [] }
+        default:
+            return state
+    }
+        
+}
 
 export default function useFetchJobs(params, page) {
+    const [state, dispatch] = useReducer(reducer, {jobs: [], loading: true })
+
+    dispatch({ type: "hello", payload: { x: 3}})
     return {
         jobs: [],
         loading: false,
