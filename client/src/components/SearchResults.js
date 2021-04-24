516f452bb56507c43e2338e24036d6a2ef0fc642
Greg Scott <gtscott90@gmail.com>
2021-04-24 17:32:34 -0400

    added useState to SearchResults to update state based on params and page

--- client/src/components/SearchResults.js
+++ client/src/components/SearchResults.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import useFetchJobs from "./useFetchJobs";
 import { Container } from "react-bootstrap";
 
 export default function SearchResults() {
-    const { jobs, loading, error } = useFetchJobs()
+    const [params, setParams] = useState({})
+    const [page, setState] = useState(1)
+    const { jobs, loading, error } = useFetchJobs(params, page)
 return(
 <Container>
  {loading && <h1>Loading...</h1>}
