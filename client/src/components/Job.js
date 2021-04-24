7cb7e21b876fe19037cb4f14d5dd48f649e211aa
Greg Scott <gtscott90@gmail.com>
2021-04-24 18:08:53 -0400

    added company logo, view details button, and link for how to apply to job card

--- client/src/components/Job.js
+++ client/src/components/Job.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card } from "react-bootstrap";
+import { Card, Badge, Button } from "react-bootstrap";
+import ReactMarkdown from "react-markdown";
 
 export default function Job({ job }) {
     return (
@@ -10,8 +11,22 @@ export default function Job({ job }) {
                         <Card.Title>
                             {job.title} - <span className="text-muted font-weight-light">{job.company}</span>
                         </Card.Title>
+                        <Card.Subtitle className="text-muted mb-2">
+                            {new Date(job.created_at).toLocaleDateString()}
+                        </Card.Subtitle>
+                        <Badge varient="secondary" className="mr-2">{job.type}</Badge>
+                        <Badge varient="secondary" className="mr-2">{job.location}</Badge>
+                        <div style={{ wordBreak: "break-all"}}>
+                            <ReactMarkdown children={job.how_to_apply} />
+                        </div>
+                    </div>
+                    <div>
+                       <img className="d-sm-none d-md-block" height="50" alt={job.company} src={job.company_logo} />
                     </div>
                 </div>
+                <Card.Text>
+                    <Button varient="primary">View Details</Button>
+                </Card.Text>
             </Card.Body>
             
         </Card>
