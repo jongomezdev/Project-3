50c5deb00e6d93b7ddbf44f3fb99300c7b0500d5
Jonathan Gomez <jongomezdev@gmail.com>
2021-04-26 18:59:51 -0400

    address the console warnings

--- client/src/components/Newsfeed.js
+++ client/src/components/Newsfeed.js
@@ -1,81 +1,81 @@
-import React, { useRef } from "react";
-import { useStoreContext } from "../utils/GlobalState";
-import { ADD_POST, LOADING } from "../utils/actions";
-import API from "../utils/API";
-import {makeStyles, Button, TextField} from '@material-ui/core';
-import PostList from "../components/PostList";
+// import React, { useRef } from "react";
+// import { useStoreContext } from "../utils/GlobalState";
+// import { ADD_POST, LOADING } from "../utils/actions";
+// import API from "../utils/API";
+// import {makeStyles, Button, TextField} from '@material-ui/core';
+// import PostList from "../components/PostList";
 
-const useStyles = makeStyles((styles) => ({
-    // STYLING BELOW
-    form: {
-      alignItems: 'center',
-      display: 'flex',
-      flexDirection: 'column',
-      margin: styles.spacing(10),
-    },
-    submit: {
-        margin: styles.spacing(3, 0, 2),
-    },
-    posts: {
-    }
-}));
+// const useStyles = makeStyles((styles) => ({
+//     // STYLING BELOW
+//     form: {
+//       alignItems: 'center',
+//       display: 'flex',
+//       flexDirection: 'column',
+//       margin: styles.spacing(10),
+//     },
+//     submit: {
+//         margin: styles.spacing(3, 0, 2),
+//     },
+//     posts: {
+//     }
+// }));
 
-function Newsfeed() {
-    const bodyRef = useRef();
-    const [state, dispatch] = useStoreContext();
+// function Newsfeed() {
+//     const bodyRef = useRef();
+//     const [state, dispatch] = useStoreContext();
 
-    const handleFormSubmit = e => {
-        e.preventDefault();
-        console.log(bodyRef.current.value)
-        dispatch({ type: LOADING });
-        API.savePost({
-            text: bodyRef.current.value
-        }).then(result => {
-            dispatch({
-                type: ADD_POST,
-                post: result.data
-            });
-        }).catch(err => console.log(err));
-        
-        bodyRef.current.value = "";
-    }
+//     const handleFormSubmit = e => {
+//         e.preventDefault();
+//         console.log(bodyRef.current.value)
+//         dispatch({ type: LOADING });
+//         API.savePost({
+//             text: bodyRef.current.value
+//         }).then(result => {
+//             dispatch({
+//                 type: ADD_POST,
+//                 post: result.data
+//             });
+//         }).catch(err => console.log(err));
 
-    const classes = useStyles();
-    return (
-        <div>
-       <form className="classes.form" onSubmit={handleFormSubmit}> 
-            <TextField 
-            className="posts"
-            inputRef={bodyRef}
-            id="filled-full-width"
-            label="Post Here"
-            style={{ margin: 8 }}
-            placeholder="Posting..."
-            helperText="Something fun! TEST"  
-            margin="normal"
-            fullWidth
-            InputLabelProps={{
-            shrink: true,
-            }}
-            variant="filled"
-        />
-            <Button
-            type="submit"
-            color="primary"
-            variant="outlined"
-            className={classes.submit}
-            // disabled={state.loading} 
-            >Submit</Button>
-      </form> 
-      <PostList />
-      </div>
-    );
-}
-// onChange={handleInputChange} - had it inside the textinput
-// value={search} - had it inside the textinput
-// disabled={state.loading} - inside button
-/* HOOK
-useRef returns a mutable ref object whose .current property is initialized to the passed argument (initialValue). The returned object will persist for the full lifetime of the component.
-useRef() is useful for more than the ref attribute. It's handy for keeping any mutable value around similar to how you'd use instance fields in classes.
- */
-export default Newsfeed;
+//         bodyRef.current.value = "";
+//     }
+
+//     const classes = useStyles();
+//     return (
+//         <div>
+//        <form className="classes.form" onSubmit={handleFormSubmit}>
+//             <TextField
+//             className="posts"
+//             inputRef={bodyRef}
+//             id="filled-full-width"
+//             label="Post Here"
+//             style={{ margin: 8 }}
+//             placeholder="Posting..."
+//             helperText="Something fun! TEST"
+//             margin="normal"
+//             fullWidth
+//             InputLabelProps={{
+//             shrink: true,
+//             }}
+//             variant="filled"
+//         />
+//             <Button
+//             type="submit"
+//             color="primary"
+//             variant="outlined"
+//             className={classes.submit}
+//             // disabled={state.loading}
+//             >Submit</Button>
+//       </form>
+//       <PostList />
+//       </div>
+//     );
+// }
+// // onChange={handleInputChange} - had it inside the textinput
+// // value={search} - had it inside the textinput
+// // disabled={state.loading} - inside button
+// /* HOOK
+// useRef returns a mutable ref object whose .current property is initialized to the passed argument (initialValue). The returned object will persist for the full lifetime of the component.
+// useRef() is useful for more than the ref attribute. It's handy for keeping any mutable value around similar to how you'd use instance fields in classes.
+//  */
+// export default Newsfeed;
