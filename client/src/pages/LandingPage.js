5f96aa00bac8cb57ea83595e932434f9e1820700
Jonathan Gomez <jongomezdev@gmail.com>
2021-04-25 20:06:53 -0400

    add experience section

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,31 +1,49 @@
-import React from "react";
-import background from "../images/developer.jpg";
-import LandingCard from "../components/LandingCard";
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import background from '../images/developer.jpg';
+import LandingCard from '../components/LandingCard';
 import { makeStyles } from '@material-ui/core';
+import { connect } from 'react-redux';
+// import PropTypes from 'prop-types';
 
 const useStyles = makeStyles({
-    root: {
-      alignItems: "center",
-      display: "flex",
-      flexDirection: "row", 
-      left: '50%',
-      justifyContent: "center",
-      backgroundSize: 'cover',
-    },
-  });
+  root: {
+    alignItems: 'center',
+    display: 'flex',
+    flexDirection: 'row',
+    left: '50%',
+    justifyContent: 'center',
+    backgroundSize: 'cover',
+  },
+});
 
-function LandingPage() {
-    const classes = useStyles();
-    return (
-        <div className={classes.root} style={{ 
-            backgroundImage: `url(${background})`,
-            backgroundRepeat: 'no-repeat',
-            height: '91.5vh',
-            width: '100%',
-          }}>
-        <LandingCard />
-        </div>
-    )
+function LandingPage({ isAuthenticated }) {
+  const classes = useStyles();
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return (
+    <div
+      className={classes.root}
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundRepeat: 'no-repeat',
+        height: '91.5vh',
+        width: '100%',
+      }}
+    >
+      <LandingCard />
+    </div>
+  );
 }
 
-export default LandingPage;
+// LandingPage.PropTypes = {
+//   isAuthenticated: PropTypes.bool,
+// };
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps)(LandingPage);
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,9 @@
-import { CLEAR_PROFILE, GET_PROFILE, PROFILE_ERROR } from '../actions/types';
+import {
+  CLEAR_PROFILE,
+  GET_PROFILE,
+  PROFILE_ERROR,
+  UPDATE_PROFILE,
+} from '../actions/types';
 
 const initialState = {
   profile: null,
@@ -13,11 +18,13 @@ export default function (state = initialState, action) {
 
   switch (type) {
     case GET_PROFILE:
+    case UPDATE_PROFILE:
       return {
         ...state,
         profile: payload,
         loading: false,
       };
+
     case PROFILE_ERROR:
       return {
         ...state,
