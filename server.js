27b2944b068bd550301befae746b15162b5c7589
Jonathan Gomez <jongomezdev@gmail.com>
2021-04-27 16:23:43 -0400

    deploy4

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
+
 const app = express();
 
 const PORT = process.env.PORT || 3001;
@@ -17,6 +19,10 @@ app.use('/api/posts', require('./routes/api/posts'));
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/devsync', {
